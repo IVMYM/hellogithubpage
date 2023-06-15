63a24563725f6a51f0a0005f46881fff9a3440c1
IVMYM <109934747+IVMYM@users.noreply.github.com>
2023-06-15 12:13:17 +0800

    Create index.php
    首页

---
+++ index.php
@@ -0,0 +1,40 @@
+<!-- index.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>My Nav - Home</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome to My Nav</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="blog.php">Blog</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section>
+            <h2>About Me</h2>
+            <p>Insert your personal information and introduction here.</p>
+        </section>
+
+        <!-- More sections and content can be added here -->
+
+    </main>
+
+    <footer>
+        <p>&copy; 2023 My Nav. All rights reserved.</p>
+    </footer>
+
+    <script src="js/main.js"></script>
+</body>
+</html>
