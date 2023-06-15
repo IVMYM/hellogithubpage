1a80239219b7bb427abe946540037050dc0e44ab
IVMYM <109934747+IVMYM@users.noreply.github.com>
2023-06-15 12:14:48 +0800

    Create about.php

---
+++ about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>About</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+  <header>
+    <!-- Add your website navigation here -->
+  </header>
+
+  <div class="content">
+    <h1>About</h1>
+    <p>This is a personal interest website where you can find various resources and engage in conversations.</p>
+    <!-- Add more content about yourself or your website here -->
+  </div>
+
+  <footer>
+    <!-- Add your footer content here -->
+  </footer>
+</body>
+</html>
